e2d22a0efc77b731798d81beec0ffd03107b64ca
GeniusSniper <tiwei61342@gmail.com>
2021-05-18 16:14:29 -0500

    p7 stack

--- sqlzoo/lib/07_joins_exercises.rb
+++ sqlzoo/lib/07_joins_exercises.rb
@@ -40,6 +40,12 @@ end
 def ford_films
   # List the films in which 'Harrison Ford' has appeared.
   execute(<<-SQL)
+  SELECT title FROM movies
+  JOIN castings
+    ON castings.movie_id = movies.id
+  JOIN actors 
+    ON castings.actor_id = actors.id
+  WHERE actors.name = 'Harrison Ford'
   SQL
 end
 
@@ -48,12 +54,24 @@ def ford_supporting_films
   # role. [Note: the ord field of casting gives the position of the actor. If
   # ord=1 then this actor is in the starring role]
   execute(<<-SQL)
+    SELECT title FROM movies
+    JOIN castings
+      ON castings.movie_id = movies.id
+    JOIN actors 
+      ON castings.actor_id = actors.id
+    WHERE actors.name = 'Harrison Ford' AND castings.ord > 1
   SQL
 end
 
 def films_and_stars_from_sixty_two
   # List the title and leading star of every 1962 film.
   execute(<<-SQL)
+    SELECT title, name FROM movies
+    JOIN castings
+      ON castings.movie_id = movies.id
+    JOIN actors 
+      ON castings.actor_id = actors.id
+    WHERE yr = 1962 AND ord = 1
   SQL
 end
 
@@ -61,6 +79,14 @@ def travoltas_busiest_years
   # Which were the busiest years for 'John Travolta'? Show the year and the
   # number of movies he made for any year in which he made at least 2 movies.
   execute(<<-SQL)
+    SELECT yr, count(*) FROM movies
+    JOIN castings
+      ON castings.movie_id = movies.id
+    JOIN actors 
+      ON castings.actor_id = actors.id
+    WHERE name = 'John Travolta'
+    GROUP BY yr 
+    HAVING count(*) > 1
   SQL
 end
 
@@ -68,6 +94,14 @@ def andrews_films_and_leads
   # List the film title and the leading actor for all of the films 'Julie
   # Andrews' played in.
   execute(<<-SQL)
+    SELECT title, name FROM actors
+    JOIN castings
+      ON castings.actor_id = actors.id
+    JOIN movies 
+      ON castings.movie_id = movies.id
+    WHERE actors.id in (
+      SELECT id FROM movies WHERE name = 'Julie Andrews'
+    ) 
   SQL
 end
 
